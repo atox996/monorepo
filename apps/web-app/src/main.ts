99200910afa37bf77bc9ec3d24a800e62a5cfa9e
王超 <wangchao1.vendor@senseauto.com>
2025-02-07 13:00:32 +0800

    chore:  eslint

--- apps/web-app/src/main.ts
+++ apps/web-app/src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { greet } from '@my/js-utils'; // 引入 js-utils 中的 greet 函数
+import { greet } from '@my/js-utils' // 引入 js-utils 中的 greet 函数
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount('#app');
+createApp(App).mount('#app')
 
-console.log(greet('World')); // 使用 greet 函数
+console.log(greet('World')) // 使用 greet 函数
